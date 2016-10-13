eae77b3cf9ad6bf3ae74c3f5bef3bc3ab4be5204
alexpug <alexandreluu6@gmail.com>
2016-10-13 12:51:04 +0200

    starting implementation for BFS

---
+++ implementation/BFS.py
@@ -0,0 +1,110 @@
+#!/bin/usr/env python
+
+import collections
+
+#################################
+## Python implementation for
+## Breath First Search
+## Reference : http://www.redblobgames.com/pathfinding/a-star/implementation.html
+
+DIAGRAM_WALLS1 = [(21, 0),(21, 1), (21, 2),(21, 3),(21, 4),(21, 5),
+        (22, 0),(22, 1),(22, 2),(22, 3),(22, 4),(22, 5),
+        (23, 0),(23, 1),(23, 2),(23, 3),(23, 4),(23, 5),
+        (24, 4),(24, 4),(25, 4),(26, 4),(26, 5),
+        (24, 5),(24, 5),(25, 5),(26, 5)]
+
+class SimpleGraph:
+    def __init__(self):
+        self.edges = {}
+    def neighbors(self, id):
+        return self.edges[id]
+
+class SquareGrid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.wall = []
+    def in_bounds(self, id):
+        (x, y) = id
+        return 0 <= x < self.width and 0 <= self.height
+    def passable(self, id):
+        return id not in self.walls
+    def neighbors(self, id):
+        (x, y) = id
+        results = [(x+1, y), (x, y-1), (x-1, y), (x, y+1)]
+        if (x + y) % 2 == 0:
+            results.reverse()
+        results = filter(self.in_bounds, results)
+        results = filter(self.passable, results)
+        return results
+
+class Queue:
+    def __init__(self):
+        self.elements = collections.deque()
+    def empty(self):
+        return len(self.elements) == 0
+    def put(self, x):
+        self.elements.append(x)
+    def get(self):
+        return self.elements.popleft()
+
+#################################
+## The representation of the Graph
+## will be
+##      (E)
+##
+## (B)        (D)
+##
+##   (C)   (A)
+## With:
+##  (A) -> (B)
+##  (B) -> (A), (C), (D)
+##  (C) -> (A)
+##  (D) -> (E) (A)
+##  (E) -> (B)
+##
+def InitGraph():
+    ex_graph = SimpleGraph()
+    ex_graph.edges = {
+            'A': ['B'],
+            'B': ['A', 'C', 'D'],
+            'C': ['A'],
+            'D': ['E', 'A'],
+            'E': ['B']
+            }
+    return ex_graph
+
+def InitGrid():
+    ex_grid = SquareGrid(30, 15)
+    ex_grid.walls = DIAGRAM_WALLS1
+    DrawGrid(ex_grid)
+
+def DrawGrid(grid):
+    string = ''
+    for y in range(0, grid.height):
+        for x in range(0, grid.width):
+            if grid.passable((x, y)):
+                string += '.'
+            else:
+                string += '#'
+        string += '\n'
+    print(string)
+
+def BreathFirstSearch(graph, start):
+    frontier = Queue()
+    frontier.put(start)
+    visited = {}
+    visited[start] = True
+
+    while not frontier.empty():
+        current = frontier.get()
+        print("Visiting {}".format(current))
+        for next in graph.neighbors(current):
+            if next not in visited:
+                frontier.put(next)
+                visited[next] = True
+
+if __name__ == "__main__":
+    BreathFirstSearch(InitGraph(), 'A');
+    InitGrid()
+
