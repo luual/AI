be4e9b76aaec4694e5ce7447add2eb7fc1dedb15
alexpug <alexandreluu6@gmail.com>
2016-10-13 20:23:32 +0200

    fix BFS for grid

--- implementation/BFS.py
+++ implementation/BFS.py
@@ -123,8 +123,8 @@ def BreathFirstSearch2(graph, start):
         for next in graph.neighbors(current):
             if next not in came_from:
                 frontier.put(next)
-                came_from[next] = None
-        return came_from
+                came_from[next] = current
+    return came_from
 
 
 
